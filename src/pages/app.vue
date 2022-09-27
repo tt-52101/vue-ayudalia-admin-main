4acbc437a12dd9855db8be8ef20139e3e0812269
Efrain Garay A <egaray7@gmail.com>
2022-09-27 08:22:12 -0300

    init commit

---
+++ src/pages/app.vue
@@ -0,0 +1,19 @@
+<route lang="yaml">
+meta:
+  requiresAuth: true
+</route>
+
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <AppLayout>
+    <!-- Content Wrapper -->
+    <RouterView v-slot="{ Component }">
+      <Transition name="fade-fast" mode="out-in">
+        <component :is="Component" :key="route.fullPath" />
+      </Transition>
+    </RouterView>
+  </AppLayout>
+</template>
